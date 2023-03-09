952f0dc82401ab6aec93e0d6bfd43b73eac25b36
TheJBoss <79772425+TheJBoss@users.noreply.github.com>
2023-03-09 10:43:52 -0700

    Finalized Unit 1

--- account.php
+++ account.php
@@ -3,7 +3,7 @@
 session_start();
 
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: account.php");
+    header("location: login.html");
     exit;
 }
 ?>
